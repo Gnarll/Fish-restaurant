34851104df49c2faf77eeb64c8e1a582ca9d8153
Gnarll <vadimfedorovich22@mail.ru>
2022-01-27 18:16:27 +0300

    add input and implement request

---
+++ src/components/SearchBar.js
@@ -0,0 +1,42 @@
+import React from "react";
+import { View, StyleSheet, TextInput } from "react-native";
+import { Feather } from "@expo/vector-icons";
+
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
+  return (
+    <View style={styles.backgroundStyle}>
+      <Feather name="search" style={styles.iconStyle} />
+      <TextInput
+        placeholder="Search"
+        style={styles.inputStyle}
+        value={term}
+        onChangeText={onTermChange}
+        autoCapitalize="none"
+        autoCorrect={false}
+        onEndEditing={onTermSubmit}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    marginTop: 10,
+    backgroundColor: "#f0eeee",
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: "row",
+  },
+  inputStyle: {
+    flex: 1,
+    fontSize: 18,
+  },
+  iconStyle: {
+    alignSelf: "center",
+    fontSize: 35,
+    marginHorizontal: 15,
+  },
+});
+
+export default SearchBar;
---
+++ src/screens/SearchScreen.js
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import SearchBar from "../components/SearchBar";
+import yelp from "../api/yelp";
+
+const SearchScreen = () => {
+  const [term, setTerm] = useState("");
+  const [results, setResults] = useState([]);
+  const [errorMessage, setErrorMessage] = useState("");
+
+  const searchApi = async (searchTerm) => {
+    try {
+      const response = await yelp.get("/search", {
+        params: {
+          limit: 50,
+          term: searchTerm,
+          location: "san jose",
+        },
+      });
+      setResults(response.data.businesses);
+    } catch (e) {
+      setErrorMessage("Something went wrong");
+    }
+  };
+
+  return (
+    <View>
+      <SearchBar
+        term={term}
+        onTermChange={(newTerm) => setTerm(newTerm)}
+        onTermSubmit={() => searchApi(term)}
+      />
+      {errorMessage ? <Text>{errorMessage}</Text> : null}
+      <Text>
+        d {results.length} results {term}{" "}
+      </Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default SearchScreen;
